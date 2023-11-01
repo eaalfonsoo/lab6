45aa0e59b574212cfea424edd2adc600f7f26997
Kamuli <k.bahemuka@ufl.edu>
2023-10-31 22:36:38 -0400

    kamuli; decode file

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ def encode(password):
     new_password = ""
     for i in range(len(password)):
         new_password += str((int(password[i]) + 3) % 10)
-    return (new_password)
+    return new_password
+
+
+def decode(password):
+    decoded_pass = ''
+    for x in range(len(password)):
+        decoded_pass += str((int(password[x]) - 3) % 10)
+    return decoded_pass
 
 
 def main():
@@ -16,12 +23,15 @@ def main():
         option = int(input("Please enter an option: "))
 
         if option == 1:
+            user_password = input('Please enter your password to encode: ')
+            encoded = encode(user_password)
             print("Your password has been encoded and stored")
         if option == 2:
-           pass
-            #print(f"The encoded password is {encode(password)}, and the original password is {decode(password)}"
+            print(f'The encoded password is {encode(user_password)}, and the original password is {decode(encoded)}.\n')
         if option == 3:
             break
+
+
 if __name__ == "__main__":
     main()
 
